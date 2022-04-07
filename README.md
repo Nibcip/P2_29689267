15db4b16bc7d8346c8aa0f7a6013ee8edf95bee1
Nibci Pinto <101876645+Nibcip@users.noreply.github.com>
2022-04-07 13:26:19 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mi primer proyecto
+
+JEJE xd
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 
-const app = express()
+const app = express() 
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res) { 
 
-    res.send('¡¡Hola Mundoo!!')
+    res.send('Hola mundo, Nombre: Nibci Pinto, cedula: 29.689.267, Sección: 1')
 })
 
 app.listen(3000)
